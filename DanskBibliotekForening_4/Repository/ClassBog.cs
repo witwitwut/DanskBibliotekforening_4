b649ffcee70386fd603f291fb794b20be82606bd
Morten Rene Pedersen <mort193c@edu.campusvejle.dk>
2018-10-03 09:55:33 +0200

    Added Repository

---
+++ DanskBibliotekForening_4/Repository/ClassBog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClassBog
+    {
+        public ClassBog()
+        {
+
+        }
+        public ClassBog(int inId, string inIsbnNr, string inTitel, string inForfatter, string inForlag, string inGenre, string inType, decimal inPris)
+        {
+            id = inId;
+            isbnNr = inIsbnNr;
+            titel = inTitel;
+            forfatter = inForfatter;
+            forlag = inForlag;
+            genre = inGenre;
+            type = inType;
+            pris = inPris;
+        }
+        private int _id;
+        private string _isbnNr;
+        private string _titel;
+        private string _forfatter;
+        private string _forlag;
+        private string _genre;
+        private string _type;
+        private decimal _pris;
+
+        public int id
+        {
+            get { return _id; }
+            private set { _id = value; }
+        }
+        public string isbnNr
+        {
+            get { return _isbnNr; }
+            set { _isbnNr = value; }
+        }
+        public string titel
+        {
+            get { return _titel; }
+            set { _titel = value; }
+        }
+        public string forfatter
+        {
+            get { return _forfatter; }
+            set { _forfatter = value; }
+        }
+        public string forlag
+        {
+            get { return _forlag; }
+            set { _forlag = value; }
+        }
+        public string genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+        public string type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public decimal pris
+        {
+            get { return _pris; }
+            set { _pris = value; }
+        }
+
+    }
+}
---
+++ DanskBibliotekForening_4/Repository/ClassPerson.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClassPerson
+    {
+        private int _id;
+        private string _navn;
+        private string _adresse;
+        private string _telefon;
+        private string _mail;
+        private int _rolle;
+        public ClassPerson()
+        {
+            id = 0;
+            navn = "";
+            adresse = "";
+            telefon = "";
+            mail = "";
+            rolle = 0;
+        }
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+        public string adresse
+        {
+            get { return _adresse; }
+            set { _adresse = value; }
+        }
+        public string telefon
+        {
+            get { return _telefon; }
+            set { _telefon = value; }
+        }
+        public string mail
+        {
+            get { return _mail; }
+            set { _mail = value; }
+        }
+        public int rolle
+        {
+            get { return _rolle; }
+            set { _rolle = value; }
+        }
+    }
+}
