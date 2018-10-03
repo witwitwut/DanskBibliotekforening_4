5b6f59d7d3bcc3d286bec2378aef596a63566b6b
Tobias Jahns Schütt <tobi7168@edu.campusvejle.dk>
2018-10-03 09:49:29 +0200

    Lavet IO Classes

---
+++ DanskBibliotekForening_4/IO/ClassDB.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IO
+{
+    public class ClassDB
+    {
+        private SqlConnection myConnection;
+
+        public ClassDB()
+        {
+
+        }
+        public ClassDB(string inConString)
+        {
+            SetCon(inConString);
+        }
+        protected void SetCon(string strCon)
+        {
+            myConnection = new SqlConnection(strCon);
+        }
+
+
+        protected void OpenDB()
+        {
+            try
+            {
+                if (myConnection != null && myConnection.State == ConnectionState.Closed)
+                {
+                    myConnection.Open();
+                }
+                else
+                {
+                    if (myConnection == null)
+                    {
+                        MessageBox.Show("Der er ikke oprettet nogen 'Connectionstring'");
+                    }
+                    else
+                    {
+                        CloseDB();
+                        OpenDB();
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+        }
+        protected void CloseDB()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+        }
+
+        protected void FunctionExecuteNonQuery(string strSql)
+        {
+            try
+            {
+                OpenDB();
+                using (SqlCommand com = new SqlCommand(strSql, myConnection))
+                {
+                    com.ExecuteNonQuery();
+                }
+                CloseDB();
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        protected DataTable DbReturnDataTable(string strsql)
+        {
+            DataTable dtRes = new DataTable();
+            try
+            {
+                OpenDB();
+                using (SqlCommand com = new SqlCommand(strsql, myConnection))
+                using (SqlDataAdapter da = new SqlDataAdapter(com))
+                {
+                    OpenDB();
+                    da.Fill(dtRes);
+                }
+                CloseDB();
+
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            return dtRes;
+        }
+        protected string DbReturnString(string strSql)
+        {
+            string res = "";
+            try
+            {
+                OpenDB();
+                using (SqlCommand com = new SqlCommand(strSql, myConnection))
+                using (SqlDataAdapter da = new SqlDataAdapter(com))
+                {
+                    SqlDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        res = reader.GetValue(0).ToString();
+                    }
+                }
+                CloseDB();
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            return res;
+        }
+
+        protected List<string> DbReturnListString(string strSql)
+        {
+            List<string> ListRes = new List<string>();
+
+            try
+            {
+                OpenDB();
+                using (SqlCommand com = new SqlCommand(strSql, myConnection))
+                {
+                    com.CommandText = strSql;
+                    using (SqlDataReader reader = com.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                ListRes.Add(reader.GetValue(i).ToString());
+                            }
+                        }
+                    }
+                }
+                CloseDB();
+            }
+            catch (SqlException)
+            {
+
+                throw;
+            }
+            return ListRes;
+        }
+
+        protected bool DbReturnBool(string strSql)
+        {
+            bool res = false;
+            try
+            {
+                OpenDB();
+                using (SqlCommand com = new SqlCommand(strSql, myConnection))
+                {
+                    SqlDataReader reader = com.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        res = Convert.ToBoolean(reader.GetValue(0).ToString());
+                    }
+                }
+                CloseDB();
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            return res;
+        }
+    }
+}
---
+++ DanskBibliotekForening_4/IO/ClassDbfDB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Repository;
+
+namespace IO
+{
+    public class ClassDbfDB : ClassDB
+    {
+        public ClassDbfDB()
+        {
+            SetCon(@"Server=10.205.44.39,49172;Database=DBF;User Id=AspIT;Password=Server2012;");
+        }
+
+        public ObservableCollection<ClassBog> GetAllBooksLike(string search)
+        {
+            ObservableCollection<ClassBog> obcBooks = new ObservableCollection<ClassBog>();
+            DataTable dt = DbReturnDataTable("SELECT Books.id, Titel.titel, ISBNnr.isbnNr, Type.TypeNavn, Forfatter.forfatter, Forlag.forlagsNavn, Genre.genreType, Books.pris" +
+                                             "FROM Books INNER JOIN Titel ON Books.titelID = Titel.id" +
+                                             "INNER JOIN Type ON Books.typeID = Type.id " +
+                                             "INNER JOIN Forfatter ON Books.forfatterID = Forfatter.id " +
+                                             "INNER JOIN Forlag ON Books.forlagID = Forlag.id " +
+                                             "INNER JOIN Genre ON Books.genreID = Genre.id " +
+                                             "INNER JOIN ISBNnr ON Books.isbnID = ISBNnr.id" +
+                                             "WHERE Titel.id LIKE '%" + search + "%'");
+            foreach (DataRow row in dt.Rows)
+            {
+                obcBooks.Add(new ClassBog(Convert.ToInt32(row["id"]),
+                                          row["titel"].ToString(),
+                                          row["isbnNr"].ToString(),
+                                          row["TypeNavn"].ToString(),
+                                          row["forfatter"].ToString(),
+                                          row["forlagsNavn"].ToString(),
+                                          row["genreType"].ToString(),
+                                          Convert.ToDecimal(row["pris"])));
+            }
+
+            return obcBooks;
+        }
+
+        public ObservableCollection<ClassBog> GetAllBooksLendToUser(string id)
+        {
+            ObservableCollection<ClassBog> obcLendBooks = new ObservableCollection<ClassBog>();
+            DataTable dt = DbReturnDataTable("SELECT Books.id, ISBNnr.isbnNr, Titel.titel, Forfatter.forfatter, Forlag.forlagsNavn, Genre.genreType, Type.TypeNavn, Books.pris" +
+                                             "FROM Books INNER JOIN ISBNnr ON Books.isbnID = ISBNnr.id " +
+                                             "INNER JOIN Titel ON Books.titelID = Titel.id " +
+                                             "INNER JOIN Forfatter ON Books.forfatterID = Forfatter.id " +
+                                             "INNER JOIN Forlag ON Books.forlagID = Forlag.id " +
+                                             "INNER JOIN Genre ON Books.genreID = Genre.id " +
+                                             "INNER JOIN Type ON Books.typeID = Type.id " +
+                                             "INNER JOIN Person ON Books.id = Person.id " +
+                                             "WHERE Person.id = '" + id + "'");
+            foreach (DataRow row in dt.Rows)
+            {
+                obcLendBooks.Add(new ClassBog(Convert.ToInt32(row["id"]),
+                                              row["titel"].ToString(),
+                                              row["isbnNr"].ToString(),
+                                              row["TypeNavn"].ToString(),
+                                              row["forfatter"].ToString(),
+                                              row["forlagsNavn"].ToString(),
+                                              row["genreType"].ToString(),
+                                              Convert.ToDecimal(row["pris"])));
+            }
+
+            return obcLendBooks;
+        }
+
+        public void UpdateTheLendingStatus(string id, bool status)
+        {
+            try
+            {
+                string sqlString = "UPDATE UdlaansStatus SET status = " + status + " WHERE id = '" + id + "'";
+                FunctionExecuteNonQuery(sqlString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fejl i DB kommunikationen", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public ClassPerson GetUser(string userID, string password)
+        {
+            ClassPerson cp = new ClassPerson();
+            string id = DbReturnString("SELECT id FROM Access WHERE cprNr = '" + userID + "', password = '" + password + "'");
+            if (id != "")
+            {
+                DataTable dt = DbReturnDataTable("SELECT Person.id, Person.navn, Person.adresse, Person.rolle, PersonMail.mailAdr, PersonTelefon.telefonnummer" +
+                                                 "FROM Access INNER JOIN BrugerRolle ON Access.id = BrugerRolle.id " +
+                                                 "INNER JOIN Person ON Access.userId = Person.id AND BrugerRolle.id = Person.rolle " +
+                                                 "INNER JOIN PersonMail ON Person.id = PersonMail.personID " +
+                                                 "INNER JOIN PersonTelefon ON Person.id = PersonTelefon.personID");
+            }
+            return cp;
+        }
+    }
+}
